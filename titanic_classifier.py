710b2d1aba325206d2b4b652f0ff71841d56a31d
Trent Fowler <fowlertm9@gmail.com>
2019-12-03 09:49:55 -0700

    finished up the titanic random forest classifier

--- titanic_classifier.py
+++ titanic_classifier.py
@@ -5,6 +5,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import FeatureUnion
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+
 
 TITANIC_PATH = os.path.join("/Users/Fowler/Desktop", "HandsOnML", "data", "titanic")
 
@@ -14,6 +18,8 @@ def load_titanic_data(filename, titanic_path=TITANIC_PATH):
     return pd.read_csv(csv_path)
 
 train_data = load_titanic_data("train.csv")
+train_data["AgeBucket"] = train_data["Age"] // 15 * 15
+train_data["RelativesOnboard"] = train_data["SibSp"] + train_data["Parch"]
 test_data = load_titanic_data("test.csv")
 
 
@@ -50,3 +56,7 @@ preprocess_pipeline = FeatureUnion(transformer_list=[
 
 X_train = preprocess_pipeline.fit_transform(train_data)
 y_train = train_data["Survived"]
+
+forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+forest_scores = cross_val_score(forest_clf, X_train, y_train, cv=10)
+print(train_data[["RelativesOnboard", "Survived"]].groupby(['RelativesOnboard']).mean())
